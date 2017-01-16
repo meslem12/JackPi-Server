83617895a56f0480269324b33f8dfe83367f9f7d
Levin Messing <levin@inode.at>
2017-01-16 22:23:41 +0100

    calculating electronics

--- src/jackpi/server/JackPiServer.java
+++ src/jackpi/server/JackPiServer.java
@@ -1,5 +1,6 @@
 package jackpi.server;
 
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -11,6 +12,14 @@ import java.net.Socket;
 public class JackPiServer
 {
 
+  /*
+  * --Protokoll--
+  * Connection opened
+  * Client schickt: "R1-R2" (100-100)
+  * Server antwortet "Ergebnis" (50)
+  * Connection closed
+  *
+   */
   /**
    * @param args the command line arguments
    */
@@ -25,9 +34,24 @@ public class JackPiServer
         try (Socket socket = server.accept())
         {
           System.out.println("Connected");
+
           OutputStream os = socket.getOutputStream();
-          os.write("I am here".getBytes());
+          InputStream is = socket.getInputStream();
+
+          byte[] tmp = new byte[50];
+          is.read(tmp);
+
+          String[] array = new String(tmp).split("-");
+
+          double r1 = Double.parseDouble(array[0].replace(',','.'));
+          double r2 = Double.parseDouble(array[1].replace(',','.'));
+          
+          String res = String.format("%.2f", (1/(1/r1 + 1/r2)));
+          
+          os.write(res.getBytes());
           os.flush();
+          System.out.println("Data sent: " + res);
+          socket.close();
         }
         System.out.println("Disconnected");
       }
